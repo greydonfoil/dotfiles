bcb1276ae61ff5c18fad298bc80cd66ae1f4618b
Greydon Foil <greydonfoil@gmail.com>
2016-06-05 14:28:32 -0400

    minor changes to move i3status config file

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@ ln -s ~/Dropbox/configFiles/.bash_profile ~/.bash_profile
 ln -s ~/Dropbox/configFiles/.bashrc ~/.bashrc
 ln -s ~/Dropbox/configFiles/.emacs ~/.emacs
 ln -s ~/Dropbox/configFiles/.i3 ~/.i3
-ln -s ~/Dropbox/configFiles/.i3status ~/.i3status
 ln -s ~/Dropbox/configFiles/.screenrc ~/.screenrc
