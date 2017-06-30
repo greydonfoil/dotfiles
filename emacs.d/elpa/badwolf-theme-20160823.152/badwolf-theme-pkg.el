b00cbb229f0416808828808108c4eb1bea448a06
Greydon Foil <gfoil@meshrobotics.com>
2017-06-30 15:21:57 -0400

    Added some elpa packages

---
+++ emacs.d/elpa/badwolf-theme-20160823.152/badwolf-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "badwolf-theme" "20160823.152" "Bad Wolf color theme" '((emacs "24")) :url "https://github.com/bkruczyk/badwolf-emacs" :keywords '("themes"))
---
+++ emacs.d/elpa/counsel-20170410.24/counsel-pkg.el
@@ -0,0 +1,1 @@
+(define-package "counsel" "20170410.24" "Various completion functions using Ivy" (quote ((emacs "24.3") (swiper "0.9.0"))))
